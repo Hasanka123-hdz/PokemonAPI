79b8f13e38fbe3fdf69a82baf27e4de05ce873c3
Hasanka123-hdz <hasankadeshapriya97@gmail.com>
2024-12-10 14:02:44 +0530

    Add project files.

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PokemonAPI.Dto;
+using PokemonAPI.Models;
+
+namespace PokemonAPI.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() {
+            CreateMap<Pokemon, PokemonDto>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
+        }
+    }
+}
